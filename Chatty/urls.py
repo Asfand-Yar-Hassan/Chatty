491a56aa364c0c6b650795a2ecf0bb8e9c6f6966
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-04-11 17:37:42 -0400

    login and registration

--- Chatty/urls.py
+++ Chatty/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from chatty_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_user, name='login')
+
 ]
--- chatty_app/views.py
+++ chatty_app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import User, Message
 
-# Create your views here.
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['username']
+        password = data['password']
+        email = data['email']
+        try:
+            User.objects.create_user(username, email, password)
+            return JsonResponse({"message": f"user created successfully: {username}"}, status=201)
+        except:
+            return JsonResponse({"error": "Username already exists"}, status=409)
+    else:
+        return JsonResponse({"error": "Method not allowed"}, status=405)
+
+
+@csrf_exempt
+def login_user(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data['username']
+        password = data['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"message": f"Welcome, {username}"}, status=200)
+        else:
+            return JsonResponse({"error": "Invalid credentials"}, status=404)
+    else:
+        return JsonResponse({"error": "Method not allowed"}, status=405)
