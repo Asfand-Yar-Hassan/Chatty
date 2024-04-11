4db6515cfe195d87292265b5d8a9f3fce5b9d330
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-04-11 17:36:50 -0400

    update models

--- chatty_app/models.py
+++ chatty_app/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class ChatRoom(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
