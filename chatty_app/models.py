806cb93e36d9720ee3be3045723db367c1660375
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-04-11 19:26:09 -0400

    field to keep count of joined users

--- chatty_app/models.py
+++ chatty_app/models.py
@@ -4,9 +4,15 @@ from django.db import models
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=100)
+    users = models.ManyToManyField(User, related_name="chatrooms")
+    user_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
+    def update_user_count(self):
+        self.user_count = self.users.count()
+        self.save()
 
 
 class Message(models.Model):
