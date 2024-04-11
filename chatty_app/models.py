d07a283e0e01a1ad62b1628a7d12bec03d52856d
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-04-11 16:23:41 -0400

    update migrations

--- chatty_app/models.py
+++ chatty_app/models.py
@@ -2,7 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
